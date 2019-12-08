f84dcf057a2025516e99934c19a7926f12f7b28e
Paul Cockrell <paul.cockrell@freeagent.com>
2019-12-08 11:55:12 +0000

    Add Events to CMS

---
+++ src/cms/preview-templates/EventsPagePreview.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { EventsPageTemplate } from '../../templates/events-page'
+
+const EventsPagePreview = ({ entry }) => {
+  const entryEvents = entry.getIn(['data', 'events'])
+  const events = entryEvents ? entryEvents.toJS() : []
+
+  return (
+    <EventsPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      description={entry.getIn(['data', 'description'])}
+      events={events}
+    />
+  )
+}
+
+EventsPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  })
+}
+
+export default EventsPagePreview

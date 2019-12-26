259c2ce8b68ea64eb15b9abae3a925f87ca8421c
Paul Cockrell <paul.cockrell@freeagent.com>
2019-12-26 18:11:49 +0000

    Add club to result participants

--- src/components/Events.js
+++ src/components/Events.js
@@ -19,14 +19,18 @@ const modalStyles = {
     bottom: "auto",
     maxWidth: "960px",
     margin: "32px auto",
-    padding: 0,
+    padding: "10px",
     border: 0,
   }
 }
 
 const Events = ({ events_list }) => {
   const [modal, setModal] = useState(false)
-  const toggleModal = () => setModal(previousValue => !previousValue)
+  const [modalContent, setModalContent] = useState("")
+  const toggleModal = (content) => {
+    setModal(previousValue => !previousValue)
+    setModalContent(content || "")
+  }
   const modalCloseTimeout = 300
 
   return (
@@ -38,44 +42,78 @@ const Events = ({ events_list }) => {
         style={modalStyles}
         contentLabel="Modal"
         closeTimeoutMS={modalCloseTimeout}>
-        <div>Hello</div>
+        {modal &&
+          modalContent.map(results => (
+            <div key={v4()}>
+              <h1 className="has-text-weight-semibold is-size-5 heading">{results.group}</h1>
+              <div className="table-container">
+                <table className="table is-bordered is-striped is-fullwidth">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Club</th>
+                      <th>Score</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {results && results.participants.map(participant => (
+                      <tr key={v4()}>
+                        <td>
+                          {participant.name}
+                        </td>
+                        <td>
+                          {participant.club}
+                        </td>
+                        <td>
+                          {participant.score}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ))
+        }
       </Modal>
-      <table className="table is-bordered is-striped is-fullwidth">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Club</th>
-            <th>Type</th>
-            <th>League</th>
-            <th>Results</th>
-          </tr>
-        </thead>
-        <tbody>
-          {events_list.map(event => (
-            <tr key={v4()}>
-              <td>
-                {moment(event.date).format('ll')}
-              </td>
-              <td>
-                {event.club}
-              </td>
-              <td>
-                {event.type}
-              </td>
-              <td>
-                {event.league}
-              </td>
-              <td className="has-text-centered">
-                {event.results ? (
-                  <button className="button is-primary" onClick={() => toggleModal()}>
-                    View results
-                  </button>
-                ) : <i>Not posted</i>}
-              </td>
+      <div className="table-container">
+        <table className="table is-bordered is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Club</th>
+              <th>Type</th>
+              <th>League</th>
+              <th>Results</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {events_list.map(event => (
+              <tr key={v4()}>
+                <td>
+                  {moment(event.date).format('ll')}
+                </td>
+                <td>
+                  {event.club}
+                </td>
+                <td>
+                  {event.type}
+                </td>
+                <td>
+                  {event.league}
+                </td>
+                <td className="has-text-centered">
+                  {event.results ? (
+                    <button className="button is-primary" onClick={() => toggleModal(event.results)}>
+                      View results
+                    </button>
+                  ) : <i>Not posted</i>}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }

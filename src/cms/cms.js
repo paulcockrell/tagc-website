f72cc967afa49f8024426ada353a4a16e3d1db4b
Paul Cockrell <paul.cockrell@freeagent.com>
2020-01-17 18:32:42 +0000

    Remove blog and products

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,6 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import LinksPagePreview from './preview-templates/LinksPagePreview'
 import BasicPagePreview from './preview-templates/BasicPagePreview'
@@ -13,7 +12,6 @@ CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('reports', BasicPagePreview)
 CMS.registerPreviewTemplate('membership', BasicPagePreview)
 CMS.registerPreviewTemplate('committee', BasicPagePreview)
